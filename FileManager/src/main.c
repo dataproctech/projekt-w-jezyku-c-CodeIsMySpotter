f7a31191985bd6e84c9aeec87be1de073e589fa0
CodeIsMySpotter <codeismyspotter@gmail.com>
2025-04-23 18:03:00 +0200

    #Issue1

--- FileManager/src/main.c
+++ FileManager/src/main.c
@@ -1,31 +1,34 @@
+#include <GTK/gtk.h>
 #include <stdio.h>
-#include <glfw3.h>
-#include <GL/gl.h>
-#include "window_utils.h"
-#include "glfw_utils.h"
+#include <stdlib.h>
 
 
 
-// Main function is the entry point of the program
-// It initializes GLFW, creates a window, sets up OpenGL, and enters the event loop
-// Finally, it cleans up and terminates GLFW
-// Returns 0 on success, -1 on failure
+
+
+
 
 int main() {
-    if (initialize_glfw() != 0) {
-        return -1;
-    }
 
-    Window window = create_window_struct(1000, 600, "Hello World");
-    if (!window.window) {
-        return -1;
-    }
+    // Initialize GTK
+    gtk_init(NULL, NULL);
+    // Create a new window
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "File Manager");
+    gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
+    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+    gtk_window_set_resizable(GTK_WINDOW(window), TRUE);
+    gtk_window_set_icon_from_file(GTK_WINDOW(window), "icon.png", NULL);
+    // Connect the destroy event to close the application
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-    setup_opengl();
-    event_loop(window.window);
 
-    printf("Hello, World!\n");
-    glfwDestroyWindow(window.window);
-    glfwTerminate();
+    // start event loop
+    gtk_widget_show_all(window);
+    gtk_main();
+    // Clean up and exit
+    gtk_widget_destroy(window);
+    gtk_main_quit();
+    // Free any resources if needed
     return 0;
 }
